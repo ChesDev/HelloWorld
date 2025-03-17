d94131e6184ba005394d465df818ba81233e40b9
ChesDev <hexakiller@yandex.ru>
2025-03-17 12:22:19 +0500

    redid equals and hashCode for all fields of the class

--- src/Book.java
+++ src/Book.java
@@ -37,12 +37,12 @@ public class Book {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return Objects.equals(title, book.title);
+        return publishingYear == book.publishingYear && Objects.equals(title, book.title) && Objects.equals(author, book.author);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(title);
+        return Objects.hash(title, author, publishingYear);
     }
 }
 
